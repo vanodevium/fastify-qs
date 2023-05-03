773aa74deb84477c02fda0efe0df7cbb0efca997
Vano Devium <webdevium@gmail.com>
2023-05-03 14:31:17 +0300

    v4.0.2

--- plugin.d.ts
+++ plugin.d.ts
@@ -1,10 +1,10 @@
-import type { FastifyPluginCallback } from 'fastify'
-import type { IParseOptions } from 'qs'
+import type { FastifyPluginCallback } from "fastify";
+import type { IParseOptions } from "qs";
 
 export interface QsPluginOptions extends IParseOptions {
-  disabled?: boolean,
-  disablePrefixTrim?: boolean,
+  disabled?: boolean;
+  disablePrefixTrim?: boolean;
 }
 
-declare const qsPlugin: FastifyPluginCallback<QsPluginOptions>
-export default qsPlugin
+declare const qsPlugin: FastifyPluginCallback<QsPluginOptions>;
+export default qsPlugin;
--- plugin.js
+++ plugin.js
@@ -1,28 +1,29 @@
-'use strict'
+"use strict";
 
-const packageJson = require('./package.json')
-const fp = require('fastify-plugin')
-const qs = require('qs')
+const packageJson = require("./package.json");
+const fp = require("fastify-plugin");
+const qs = require("qs");
 
 const plugin = (fastify, options, next) => {
-  fastify.addHook('onRequest', (request, reply, done) => {
+  fastify.addHook("onRequest", (request, reply, done) => {
     if (options && options.disabled) {
-      return done()
+      return done();
     }
-    const rawUrl = request.raw.url
-    let url = rawUrl
+    const rawUrl = request.raw.url;
+    let url = rawUrl;
     if (!(options && options.disablePrefixTrim)) {
-      url = rawUrl.replace(/\?{2,}/, '?')
+      url = rawUrl.replace(/\?{2,}/, "?");
     }
-    const querySymbolIndex = url.indexOf('?')
-    const query = querySymbolIndex !== -1 ? url.slice(querySymbolIndex + 1) : ''
-    request.query = qs.parse(query, options)
-    done()
-  })
-  next()
-}
+    const querySymbolIndex = url.indexOf("?");
+    const query =
+      querySymbolIndex !== -1 ? url.slice(querySymbolIndex + 1) : "";
+    request.query = qs.parse(query, options);
+    done();
+  });
+  next();
+};
 
 module.exports = fp(plugin, {
-  fastify: '^4.0.0',
-  name: packageJson.name
-})
+  fastify: "^4.0.0",
+  name: packageJson.name,
+});
